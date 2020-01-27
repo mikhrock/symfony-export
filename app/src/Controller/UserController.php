215e60c8913053d1bfdd380e133aeedba834c9a6
mikhrock <mikhail.kolker@gmail.com>
2020-01-27 13:53:10 +0200

    added front end and users pagination

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +14,25 @@ class UserController extends AbstractController
 {
     /**
      * @Route("/")
+     * @IsGranted("ROLE_USER")
+     * @param UserRepository $repository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
      */
-    public function index()
+    public function index(UserRepository $repository, Request $request, PaginatorInterface $paginator)
     {
-        $number = random_int(0, 100);
+        $users = $repository->findAll();
+
+        $pagination = $paginator->paginate(
+            $repository->createQueryBuilder('u')->getQuery(),
+            $request->query->getInt('page', 1),
+            10
+        );
 
         return $this->render('user/users-list.html.twig', [
-            'number' => $number,
+            'users' => $users,
+            'pagination' => $pagination,
         ]);
     }
 }
